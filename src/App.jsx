01701ce3826be9bbd9fd85474efe0aafd114ef6a
nethuchathuu <155764628+nethuchathuu@users.noreply.github.com>
2025-06-15 22:51:46 +0530

    update5

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-// App.js or wherever your routing is set up
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import RecipeFinder from './components/RecipeFinder';
 import ChatWithChef from './components/ChatWithChef';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/recipes" element={<RecipeFinder />} />
-        <Route path="/chat" element={<ChatWithChef />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/recipes" element={<RecipeFinder />} />
+      <Route path="/chat" element={<ChatWithChef />} />
+    </Routes>
   );
 }
 
